0eb466369d2b9788196bb01672db4b5fcf560bb1
ayagima-k.win <a.kei.0224@gmail.com>
2022-09-30 23:50:30 +0900

    未選択UserControlを追加

---
+++ WpfSampleApp/WpfSampleApp.Controls/NoneUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfSampleApp.Controls
+{
+    /// <summary>
+    /// NoneUserControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NoneUserControl : UserControl
+    {
+        public NoneUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
